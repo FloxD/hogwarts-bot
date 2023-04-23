5b4e7babdc8b58c61385e442286c2f1e619f2292
FloxD <>
2023-04-23 13:41:13 +0200

    disable incendio

--- src/main/kotlin/com/floxd/hogwartsbot/service/DiscordMessageListener.kt
+++ src/main/kotlin/com/floxd/hogwartsbot/service/DiscordMessageListener.kt
@@ -5,14 +5,18 @@ import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
-class DiscordMessageListener(val effectService: EffectService) : ListenerAdapter() {
+class DiscordMessageListener(val effectService: EffectService,
+                             @Value("\${incendio.enabled}") private val incendioEnabled: Boolean) : ListenerAdapter() {
+
     private val LOGGER = LoggerFactory.getLogger(DiscordMessageListener::class.java)
+
     override fun onMessageReceived(event: MessageReceivedEvent) {
         try {
-            if (effectService.checkForEffect(SpellEnum.INCENDIO, event.author.id).isAffected) {
+            if (effectService.checkForEffect(SpellEnum.INCENDIO, event.author.id).isAffected && incendioEnabled) {
                 //React to the message with a fire emoji.
                 event.message.addReaction(Emoji.fromUnicode("U+1F525")).queue()
             }
